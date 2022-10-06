60ef0cc5bc4628f61e910c9fcb8fa501eeb0343f
thisIsHyeongSeokLee <oddvapor@gmail.com>
2022-10-07 00:00:24 +0900

    initial commit

---
+++ meal/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+# 레시피(recipe)와 원재료(groceries) 클래스에서 Meal 클래스에 Foreignkey로 모델을 불러와 식단을 작성시킬 예정
+
+     
+class Groceries(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    groceries = models.ForeignKey(Groceries, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+class Meal(models.Model):
+    time = models.DateTimeField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.time
+    
+
+
+     
