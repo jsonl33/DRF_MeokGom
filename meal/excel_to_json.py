c2ccb42d1ff8a968838447d98b5c150746acc606
thisIsHyeongSeokLee <oddvapor@gmail.com>
2022-10-13 18:05:58 +0900

    딕셔너리가 너무 커 DB를 영양소를 최소화하고 음식 가짓수를 99개로 축소(임시)

--- meal/excel_to_json.py
+++ meal/excel_to_json.py
@@ -2,37 +2,9 @@ import openpyxl
 import json
 
 
-# class food_data():
-#     excel_file = 'staticfiles/통합_식품영양성분DB_음식_20221011.xlsx'
-#     json_file = 'staticfiles/food_data.json'
-#     wb = openpyxl.load_workbook(excel_file, read_only=True)
-    
-#     sheet = wb.worksheets[0]
-    
-#     key_list = []
-#     for col_num in range(1, sheet.max_column + 1):
-#         key_list.append(sheet.cell(row=4, column=col_num).value)
-    
-#     food_dict = {}
-#     key_index = 5
-#     for row_num in range(2, sheet.max_row + 1):
-#         tmp_dict = {}
-#         for col_num in range(1, sheet.max_column + 1):
-#             val = sheet.cell(row=row_num, column=col_num).value
-#             tmp_dict[key_list[col_num - 1]] = val
-        
-#         food_dict[tmp_dict[key_list[key_index]]] = tmp_dict
-    
-#     wb.close()
-
-#     with open(json_file, 'w', encoding='utf-8') as fp:
-#         json.dump(food_dict, fp, indent=4, ensure_ascii=False)
-
-
-        
-class fisheries_data():
-    excel_file = 'staticfiles/통합_식품영양성분DB_수산물_20221011.xlsx'
-    json_file = 'staticfiles/fisheries_data.json'
+class food_data():
+    excel_file = 'staticfiles/foodDB99.xlsx'
+    json_file = 'staticfiles/food_data.json'
     wb = openpyxl.load_workbook(excel_file, read_only=True)
     
     sheet = wb.worksheets[0]
@@ -41,20 +13,47 @@ class fisheries_data():
     for col_num in range(1, sheet.max_column + 1):
         key_list.append(sheet.cell(row=1, column=col_num).value)
     
-    fish_dict = {}
-    key_index = 5
+    food_dict = {}
+    key_index = 3
     for row_num in range(2, sheet.max_row + 1):
         tmp_dict = {}
         for col_num in range(1, sheet.max_column + 1):
             val = sheet.cell(row=row_num, column=col_num).value
             tmp_dict[key_list[col_num - 1]] = val
         
-        fish_dict[tmp_dict[key_list[key_index]]] = tmp_dict
-    
+        food_dict[tmp_dict[key_list[key_index]]] = tmp_dict
     wb.close()
 
     with open(json_file, 'w', encoding='utf-8') as fp:
-        json.dump(fish_dict, fp, indent=4, ensure_ascii=False)
+        json.dump(food_dict, fp, indent=4, ensure_ascii=False)
+
+
+        
+# class fisheries_data():
+#     excel_file = 'staticfiles/통합_식품영양성분DB_수산물_20221011.xlsx'
+#     json_file = 'staticfiles/fisheries_data.json'
+#     wb = openpyxl.load_workbook(excel_file, read_only=True)
+    
+#     sheet = wb.worksheets[0]
+    
+#     key_list = []
+#     for col_num in range(1, sheet.max_column + 1):
+#         key_list.append(sheet.cell(row=1, column=col_num).value)
+    
+#     fish_dict = {}
+#     key_index = 5
+#     for row_num in range(2, sheet.max_row + 1):
+#         tmp_dict = {}
+#         for col_num in range(1, sheet.max_column + 1):
+#             val = sheet.cell(row=row_num, column=col_num).value
+#             tmp_dict[key_list[col_num - 1]] = val
+        
+#         fish_dict[tmp_dict[key_list[key_index]]] = tmp_dict
+    
+#     wb.close()
+
+#     with open(json_file, 'w', encoding='utf-8') as fp:
+#         json.dump(fish_dict, fp, indent=4, ensure_ascii=False)
         
         
         
